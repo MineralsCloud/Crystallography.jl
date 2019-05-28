eaecb5b2522acf419951dc6a09b8c7ed20c2dfaf
Qi Zhang <singularitti@outlook.com>
2019-05-28 15:12:10 -0400

    Add type annotations

--- src/Directions.jl
+++ src/Directions.jl
@@ -52,7 +52,7 @@ function MetricTensor{S}(v1::AbstractVector, v2::AbstractVector, v3::AbstractVec
     vecs = (v1, v2, v3)
     MetricTensor{S}(map(x->dot(x...), Iterators.product(vecs, vecs)))
 end
-function MetricTensor{RealSpace}(a, b, c, α, β, γ)
+function MetricTensor{RealSpace}(a::Real, b::Real, c::Real, α::Real, β::Real, γ::Real)
     g12 = a * b * cos(γ)
     g13 = a * c * cos(β)
     g23 = b * c * cos(α)
