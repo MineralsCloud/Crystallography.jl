f27835ed0f266923da6960700cff7971ad4307e4
Qi Zhang <singularitti@outlook.com>
2019-05-27 21:38:30 -0400

    Fix bus in SeitzOperators.jl

--- src/SeitzOperators.jl
+++ src/SeitzOperators.jl
@@ -11,32 +11,49 @@ julia>
 """
 module SeitzOperators
 
-using LinearAlgebra
+using LinearAlgebra: diagm, I
+
+using LuxurySparse: IMatrix
 
 export SeitzOperator,
     IdentityOperator,
     TranslationOperator,
     PointSymmetryOperator
 
-abstract type SeitzOperator{T} end
+abstract type SeitzOperator{T <: AbstractMatrix} end
 
 struct IdentityOperator{T} <: SeitzOperator{T}
     m::T
+    function IdentityOperator{T}(m) where {T}
+        size(m) == (4, 4) || throw(DimensionMismatch("The operator must be of size 4x4!"))
+        m == I || error("The matrix is not an identity matrix!")
+        new(m)
+    end
 end
-IdentityOperator() = IdentityOperator(ones(4))
+IdentityOperator(m::T) where {T} = IdentityOperator{T}(m)
 
 struct TranslationOperator{T} <: SeitzOperator{T}
     m::T
-    function TranslationOperator(vec::AbstractVector{T}) where {T}
-        vcat(hcat(diagm(0 => ones(T, 3)), vec), lastrow(T))
+    function TranslationOperator{T}(m) where {T}
+        size(m) == (4, 4) || throw(DimensionMismatch("The operator must be of size 4x4!"))
+        new(m)
     end
+end
+TranslationOperator(m::T) where {T} = TranslationOperator{T}(m)
+function TranslationOperator(vec::AbstractVector{T}) where {T}
+    TranslationOperator(vcat(hcat(diagm(0 => ones(T, 3)), vec), lastrow(T)))
 end
 
 struct PointSymmetryOperator{T} <: SeitzOperator{T}
     m::T
-    function PointSymmetryOperator(mat::AbstractMatrix{T}) where {T}
-        vcat(hcat(mat, zeros(T, 3)), lastrow(T))
+    function PointSymmetryOperator{T}(m) where {T}
+        size(m) == (4, 4) || throw(DimensionMismatch("The operator must be of size 4x4!"))
+        new(m)
     end
+end
+PointSymmetryOperator(m::T) where {T} = PointSymmetryOperator{T}(m)
+function PointSymmetryOperator(mat::AbstractMatrix{T}) where {T}
+    PointSymmetryOperator(vcat(hcat(mat, zeros(T, 3)), lastrow(T)))
 end
 
 lastrow(::Type{T}) where {T} = [zeros(T, 3)... ones(T, 1)]
