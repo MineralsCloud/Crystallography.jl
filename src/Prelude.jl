8eab4562f8772b158f8d37023461059fce6b4023
Qi Zhang <singularitti@outlook.com>
2019-06-03 03:34:51 -0400

    Let `CrystalCoordinates` can add each other and multiplied by numbers

--- src/Prelude.jl
+++ src/Prelude.jl
@@ -29,6 +29,18 @@ struct CrystalCoordinates{S <: AbstractSpace,T} <: FieldVector{3,T}
 end
 CrystalCoordinates{S}(x::T, y::T, z::T) where {S,T} = CrystalCoordinates{S,T}(x, y, z)
 
+for operator in (:+, :-)
+    eval(quote
+        Base.$operator(a::CrystalCoordinates{T}, b::CrystalCoordinates{T}) where {T} = CrystalCoordinates{T}(collect(a) + collect(b))
+    end)
+end
+for operator in (:*, :/, :÷)
+    eval(quote
+        Base.$operator(n::Number, a::CrystalCoordinates{T}) where {T} = CrystalCoordinates{T}($operator(collect(a), n))
+        Base.$operator(a::T, n::Number) where {T <: CrystalCoordinates} = $operator(n, a)
+    end)
+end
+
 Base.inv(::Type{RealSpace}) = ReciprocalSpace
 Base.inv(::Type{ReciprocalSpace}) = RealSpace
 
