811d46f58dd9e7bae4364c19f43cc0c1be9500ad
Qi Zhang <singularitti@outlook.com>
2019-06-02 22:52:21 -0400

    Add TestCrystallographicDirections.jl

---
+++ test/TestCrystallographicDirections.jl
@@ -0,0 +1,46 @@
+#=
+TestCrystallographicDirections.jl:
+- Julia version: 1.0
+- Author: qz
+- Date: Jun 2, 2019
+=#
+module TestCrystallographicDirections
+
+using Test
+
+using Crystallography
+using Crystallography.CrystallographicDirections
+
+@testset "Test conversion between real `MillerIndices` and `MillerBravaisIndices`" begin
+    millerreal = [[1, 1, 1], [-1, 0, 1], [0, -1, 1], [-1, -1, 1], [1, 0, 1], [0, 1, 1]]
+    millerbravaisreal = [[1, 1, -2, 3], [-2, 1, 1, 3], [1, -2, 1, 3], [-1, -1, 2, 3], [2, -1, -1, 3], [-1, 2, -1, 3]]
+    for (x, y) in zip(millerreal, millerbravaisreal)
+        m = MillerIndices{RealSpace}(x)
+        mb = MillerBravaisIndices{RealSpace}(y)
+        @test convert(MillerBravaisIndices{RealSpace}, m) == mb
+        @test convert(MillerIndices{RealSpace}, mb) == m
+        # @test_throws ErrorException convert(MillerIndices{ReciprocalSpace}, m)
+        # @test_throws ErrorException convert(MillerBravaisIndices{ReciprocalSpace}, mb)
+        @test_throws ErrorException convert(MillerBravaisIndices{ReciprocalSpace}, m)
+        @test_throws ErrorException convert(MillerIndices{ReciprocalSpace}, mb)
+        @test_throws ErrorException convert(MillerBravaisIndices, m)
+        @test_throws ErrorException convert(MillerIndices, mb)
+    end
+end
+
+@testset "Test conversion between reciprocal `MillerIndices` and `MillerBravaisIndices`" begin
+    millerreciprocal = [[1, 0, 0], [0, 1, 0], [1, -1, 0], [-1, 0, 0], [0, -1, 0], [-1, 1, 0]]
+    millerbravaisreciprocal = [[1, 0, -1, 0], [0, 1, -1, 0], [1, -1, 0, 0], [-1, 0, 1, 0], [0, -1, 1, 0], [-1, 1, 0, 0]]
+    for (x, y) in zip(millerreciprocal, millerbravaisreciprocal)
+        m = MillerIndices{ReciprocalSpace}(x)
+        mb = MillerBravaisIndices{ReciprocalSpace}(y)
+        @test convert(MillerBravaisIndices{ReciprocalSpace}, m) == mb
+        @test convert(MillerIndices{ReciprocalSpace}, mb) == m
+        @test_throws ErrorException convert(MillerBravaisIndices{RealSpace}, m)
+        @test_throws ErrorException convert(MillerIndices{RealSpace}, mb)
+        @test_throws ErrorException convert(MillerBravaisIndices, m)
+        @test_throws ErrorException convert(MillerIndices, mb)
+    end
+end
+
+end
