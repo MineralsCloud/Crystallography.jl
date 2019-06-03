c4caf4672158c669039d16eb55e0c6b7591c8c63
Qi Zhang <singularitti@outlook.com>
2019-06-03 02:29:38 -0400

    Add a type `CrystalCoordinates`
    to replace `Translation` (It should not be used to calculate crystallographic
    things).

--- src/Prelude.jl
+++ src/Prelude.jl
@@ -11,13 +11,22 @@ julia>
 """
 module Prelude
 
+using StaticArrays: FieldVector
+
 export AbstractSpace,
     RealSpace,
-    ReciprocalSpace
+    ReciprocalSpace,
+    CrystalCoordinates
 
 abstract type AbstractSpace end
 struct RealSpace <: AbstractSpace end
 struct ReciprocalSpace <: AbstractSpace end
+
+struct CrystalCoordinates{S <: AbstractSpace,T} <: FieldVector{3,T}
+    x::T
+    y::T
+    z::T
+end
 
 Base.inv(::Type{RealSpace}) = ReciprocalSpace
 Base.inv(::Type{ReciprocalSpace}) = RealSpace
