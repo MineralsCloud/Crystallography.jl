0711b1885e67d4a2eea2f05539112ce44cdfa85b
Qi Zhang <singularitti@outlook.com>
2019-06-19 03:18:12 -0400

    Let `transformation` return `LinearMap`s
    instead of `Matrices`

--- src/CrystalFrame.jl
+++ src/CrystalFrame.jl
@@ -11,6 +11,7 @@ julia>
 """
 module CrystalFrame
 
+using CoordinateTransformations
 using StaticArrays
 
 using Crystallography
@@ -39,22 +40,22 @@ transformation(::Type{BravaisLattice{Primitive,Cubic}}, ::Type{RealSpace}, cell:
     1 0 0
     0 1 0
     0 0 1
-]
+] |> LinearMap
 transformation(::Type{BravaisLattice{BodyCentered,Cubic}}, ::Type{RealSpace}, cell::CellParameters) = cell.a / 2 * [
     1  1 1
    -1  1 1
    -1 -1 1
-]
+] |> LinearMap
 transformation(::Type{BravaisLattice{FaceCentered,Cubic}}, ::Type{RealSpace}, cell::CellParameters) = cell.a / 2 * [
    -1 0 1
     0 1 1
    -1 1 0
-]
+] |> LinearMap
 transformation(::Type{BravaisLattice{Primitive,Hexagonal}}, ::Type{RealSpace}, cell::CellParameters) = cell.a * [
     1 0 0
     -1 / 2 √3 / 2 0
     0 0 cell.c / cell.a
-]
+] |> LinearMap
 function transformation(::Type{BravaisLattice{RhombohedralCentered,Hexagonal}}, ::Type{RealSpace}, cell::CellParameters)
     r = cos(cell.α)
     tx = sqrt((1 - r) / 2)
@@ -64,26 +65,26 @@ function transformation(::Type{BravaisLattice{RhombohedralCentered,Hexagonal}}, 
         tx -ty tz
         0  2ty tz
        -tx -ty tz
-    ]
+    ] |> LinearMap
 end
 transformation(::Type{BravaisLattice{Primitive,Tetragonal}}, ::Type{RealSpace}, cell::CellParameters) = cell.a * [
     1 0 0
     0 1 0
     0 0 cell.c / cell.a
-]
+] |> LinearMap
 function transformation(::Type{BravaisLattice{BodyCentered,Tetragonal}}, ::Type{RealSpace}, cell::CellParameters)
     r = cell.c / cell.a
     cell.a / 2 * [
         1 -1 r
         1  1 r
        -1 -1 r
-    ]
+    ] |> LinearMap
 end
 transformation(::Type{BravaisLattice{Primitive,Orthorhombic}}, ::Type{RealSpace}, cell::CellParameters) = [
     cell.a 0 0
     0 cell.b 0
     0 0 cell.c
-]
+] |> LinearMap
 # TODO: BravaisLattice{CCentered,Orthorhombic}
 function transformation(::Type{BravaisLattice{FaceCentered,Orthorhombic}}, ::Type{RealSpace}, cell::CellParameters)
     a, b, c = cell.a, cell.b, cell.c
@@ -91,7 +92,7 @@ function transformation(::Type{BravaisLattice{FaceCentered,Orthorhombic}}, ::Typ
         a 0 c
         a b 0
         0 b c
-    ] / 2
+    ] / 2 |> LinearMap
 end
 function transformation(::Type{BravaisLattice{BodyCentered,Orthorhombic}}, ::Type{RealSpace}, cell::CellParameters)
     a, b, c = cell.a, cell.b, cell.c
@@ -99,7 +100,7 @@ function transformation(::Type{BravaisLattice{BodyCentered,Orthorhombic}}, ::Typ
         a  b c
        -a  b c
        -a -b c
-    ] / 2
+    ] / 2 |> LinearMap
 end
 function transformation(::Type{BravaisLattice{Primitive,Monoclinic}}, ::Type{RealSpace}, cell::CellParameters)
     a, b, c = cell.a, cell.b, cell.c
@@ -107,7 +108,7 @@ function transformation(::Type{BravaisLattice{Primitive,Monoclinic}}, ::Type{Rea
         a 0 0
         0 b 0
         c * cos(cell.β) 0 c * sin(cell.β)
-    ]
+    ] |> LinearMap
 end
 # TODO: BravaisLattice{BCentered,Monoclinic}
 # TODO: BravaisLattice{Primitive,Triclinic}
