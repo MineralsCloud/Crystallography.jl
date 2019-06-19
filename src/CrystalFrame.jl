9b0fb366c7059c2a07131577664bd2690099a096
Qi Zhang <singularitti@outlook.com>
2019-06-19 02:52:10 -0400

    Rewrite CrystalFrame.jl
    Deprecate
    1. `AbstractCoordinatesTransformation`
    2. `CrystalFromCartesian`
    3. `CartesianFromCrystal`
    Add
    1. `CellParameters`
    2. Redefine `transformation`

--- src/CrystalFrame.jl
+++ src/CrystalFrame.jl
@@ -11,18 +11,105 @@ julia>
 """
 module CrystalFrame
 
+using StaticArrays
+
 using Crystallography
 
-export AbstractCoordinatesTransformation,
-    CrystalFromCartesian,
-    CartesianFromCrystal,
+export CellParameters,
     transformation
 
-abstract type AbstractCoordinatesTransformation{T <: BravaisLattice} end
-struct CrystalFromCartesian{T} <: AbstractCoordinatesTransformation{T} end
-struct CartesianFromCrystal{T} <: AbstractCoordinatesTransformation{T} end
+struct CellParameters#{T} <: FieldVector{6,T}
+    a
+    b
+    c
+    α
+    β
+    γ
+end
+CellParameters(::Type{Triclinic}, args...) = CellParameters(args...)
+CellParameters(::Type{Monoclinic}, a, b, c, γ) = CellParameters(a, b, c, π / 2, π / 2, γ)
+CellParameters(::Type{Orthorhombic}, a, b, c) = CellParameters(a, b, c, π / 2, π / 2, π / 2)
+CellParameters(::Type{Tetragonal}, a, c) = CellParameters(a, a, c, π / 2, π / 2, π / 2)
+CellParameters(::Type{Cubic}, a) = CellParameters(a, a, a, π / 2, π / 2, π / 2)
+CellParameters(::Type{Hexagonal}, a, c) = CellParameters(a, a, c, π / 2, π / 2, 2π / 3)
+CellParameters(::Type{Trigonal}, a, c) = CellParameters(a, a, c, π / 2, π / 2, 2π / 3)
+CellParameters(::Type{BravaisLattice{RhombohedralCentered,Hexagonal}}, a, α) = CellParameters(a, a, a, α, α, α)
 
-transformation(::Type{T}, a::CrystalCoordinates) where {T <: BravaisLattice} = transformation(CartesianFromCrystal{T}, collect(a))
-transformation(::Type{T}, a::CartesianCoordinates) where {T <: BravaisLattice} = transformation(CrystalFromCartesian{T}, collect(a))
+transformation(::Type{BravaisLattice{Primitive,Cubic}}, ::Type{RealSpace}, cell::CellParameters) = cell.a * [
+    1 0 0
+    0 1 0
+    0 0 1
+]
+transformation(::Type{BravaisLattice{BodyCentered,Cubic}}, ::Type{RealSpace}, cell::CellParameters) = cell.a / 2 * [
+    1  1 1
+   -1  1 1
+   -1 -1 1
+]
+transformation(::Type{BravaisLattice{FaceCentered,Cubic}}, ::Type{RealSpace}, cell::CellParameters) = cell.a / 2 * [
+   -1 0 1
+    0 1 1
+   -1 1 0
+]
+transformation(::Type{BravaisLattice{Primitive,Hexagonal}}, ::Type{RealSpace}, cell::CellParameters) = cell.a * [
+    1 0 0
+    -1 / 2 √3 / 2 0
+    0 0 cell.c / cell.a
+]
+function transformation(::Type{BravaisLattice{RhombohedralCentered,Hexagonal}}, ::Type{RealSpace}, cell::CellParameters)
+    r = cos(cell.α)
+    tx = sqrt((1 - r) / 2)
+    ty = sqrt((1 - r) / 6)
+    tz = sqrt((1 + 2r) / 3)
+    cell.a * [
+        tx -ty tz
+        0  2ty tz
+       -tx -ty tz
+    ]
+end
+transformation(::Type{BravaisLattice{Primitive,Tetragonal}}, ::Type{RealSpace}, cell::CellParameters) = cell.a * [
+    1 0 0
+    0 1 0
+    0 0 cell.c / cell.a
+]
+function transformation(::Type{BravaisLattice{BodyCentered,Tetragonal}}, ::Type{RealSpace}, cell::CellParameters)
+    r = cell.c / cell.a
+    cell.a / 2 * [
+        1 -1 r
+        1  1 r
+       -1 -1 r
+    ]
+end
+transformation(::Type{BravaisLattice{Primitive,Orthorhombic}}, ::Type{RealSpace}, cell::CellParameters) = [
+    cell.a 0 0
+    0 cell.b 0
+    0 0 cell.c
+]
+# TODO: BravaisLattice{CCentered,Orthorhombic}
+function transformation(::Type{BravaisLattice{FaceCentered,Orthorhombic}}, ::Type{RealSpace}, cell::CellParameters)
+    a, b, c = cell.a, cell.b, cell.c
+    [
+        a 0 c
+        a b 0
+        0 b c
+    ] / 2
+end
+function transformation(::Type{BravaisLattice{BodyCentered,Orthorhombic}}, ::Type{RealSpace}, cell::CellParameters)
+    a, b, c = cell.a, cell.b, cell.c
+    [
+        a  b c
+       -a  b c
+       -a -b c
+    ] / 2
+end
+function transformation(::Type{BravaisLattice{Primitive,Monoclinic}}, ::Type{RealSpace}, cell::CellParameters)
+    a, b, c = cell.a, cell.b, cell.c
+    [
+        a 0 0
+        0 b 0
+        c * cos(cell.β) 0 c * sin(cell.β)
+    ]
+end
+# TODO: BravaisLattice{BCentered,Monoclinic}
+# TODO: BravaisLattice{Primitive,Triclinic}
 
 end
