703b64877a0143cd55cb9d7b8bea8bdb396d22ba
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-07-11 20:43:00 +0000

    build based on 52b6c93

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Crystallography.jl-1","page":"Home","title":"Crystallography.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Crystallography]","category":"page"}]
+}
