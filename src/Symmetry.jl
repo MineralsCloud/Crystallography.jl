172f45197faf1d6242787198b059d321892d3678
Qi Zhang <singularitti@outlook.com>
2020-02-23 21:10:39 -0500

    Merge all into `SeitzOperator`

--- src/Symmetry.jl
+++ src/Symmetry.jl
@@ -4,11 +4,12 @@ using LinearAlgebra: diagm, I
 
 using CoordinateTransformations: AffineMap, Translation, LinearMap
 using LibSymspg: get_symmetry
+using StaticArrays: StaticMatrix
 
 using Crystallography: CrystalCoordinates, Cell
 
-export SeitzOperator, IdentityOperator, TranslationOperator, PointSymmetryOperator
-export getsymmetry
+export SeitzOperator
+export getsymmetry, isidentity, istranslation, ispointsymmetry
 
 function getsymmetry(cell::Cell, symprec::AbstractFloat = 1e-5)
     maps, translations = get_symmetry(
@@ -23,40 +24,29 @@ end
 
 struct SeitzOperator{T}
     m::T
-    function SeitzOperator{T}(m) where {T}
-        @assert(size(m) == (4, 4), "The operator must be of size 4x4!")
-        return new(m)
-    end
 end
-SeitzOperator(m::T) where {T} = SeitzOperator{T}(m)
-function SeitzOperator(m::AffineMap)
-    linear, translation = m.linear, m.translation
-    return SeitzOperator(translation ∘ linear ∘ inv(translation))
-end # function SeitzOperator
-function SeitzOperator(s::SeitzOperator, pos::AbstractVector)
-    @assert length(pos) == 3
-    t = TranslationOperator(Translation(pos))
-    return t * s * inv(t)
-end # function SeitzOperator
-SeitzOperator(a:AbstractMatrix, pos::AbstractVector) = SeitzOperator(SeitzOperator(a), pos)
-
-IdentityOperator() = SeitzOperator(ones(Int, 4, 4))
-
-function TranslationOperator(v::AbstractVector)
-    @assert length(v) == 3
-    x = diagm(ones(eltype(v), 4))
-    x[1:3, 4] = v
-    return SeitzOperator(x)
-end # function TranslationOperator
-TranslationOperator(t::Translation) = TranslationOperator(t.translation)
-
-function PointSymmetryOperator(m::AbstractMatrix)
+SeitzOperator() = SeitzOperator(ones(Int, 4, 4))
+function SeitzOperator(m::LinearMap)
     @assert size(m) == (3, 3)
-    x = diagm(ones(eltype(m), 4))
+    x = diagm(ones(eltype(m.linear), 4))
     x[1:3, 1:3] = m
     return SeitzOperator(x)
 end # function PointSymmetryOperator
-PointSymmetryOperator(l::LinearMap) = PointSymmetryOperator(l.linear)
+function SeitzOperator(t::Translation)
+    @assert length(t) == 3
+    x = diagm(ones(eltype(t.translation), 4))
+    x[1:3, 4] = t
+    return SeitzOperator(x)
+end # function TranslationOperator
+SeitzOperator(m::LinearMap, t::Translation) = SeitzOperator(t ∘ m ∘ inv(t))
+SeitzOperator(a::AffineMap) = SeitzOperator(a.linear, a.translation)
+function SeitzOperator(s::SeitzOperator, pos::AbstractVector)
+    @assert length(pos) == 3
+    t = SeitzOperator(Translation(pos))
+    return t * s * inv(t)
+end # function SeitzOperator
+
+isidentity(op::SeitzOperator) = op.m == I
 
 function istranslation(op::SeitzOperator)
     m = op.m
@@ -74,15 +64,7 @@ function ispointsymmetry(op::SeitzOperator)
     return true
 end # function ispointsymmetry
 
-# Base.eltype(t::Translation) = eltype(t.translation)
-# Base.eltype(m::LinearMap) = eltype(m.linear)
-
 Base.:*(m::SeitzOperator, c::CrystalCoordinates) = CrystalCoordinates((m.m*[c; 1])[1:3])
 Base.:*(a::SeitzOperator, b::SeitzOperator) = SeitzOperator(a.m * b.m)
-
-    # Base.convert(::Type{Translation}, op::TranslationOperator) = Translation(collect(op.m[1:3, 4]))
-
-    # Base.inv(op::TranslationOperator) = TranslationOperator(inv(convert(Translation, op)))
-
 
 end # module Symmetry
