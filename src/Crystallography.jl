d147b7a1842ab95836eda81ecccdea93499c0d71
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-26 22:49:49 -0400

    Move `cellvolume` to src/arithmetics.jl

--- src/Crystallography.jl
+++ src/Crystallography.jl
@@ -11,22 +11,6 @@ import Spglib: basis_vectors
 
 include("lattice.jl")
 
-"""
-    cellvolume(a, b, c, α, β, γ)
-
-Calculates the cell volume from 6 cell parameters.
-"""
-cellvolume(a, b, c, α, β, γ) =
-    a * b * c * sqrt(sin(α)^2 - cos(β)^2 - cos(γ)^2 + 2 * cos(α) * cos(β) * cos(γ))
-"""
-    cellvolume(l::Lattice)
-    cellvolume(c::Cell)
-
-Calculates the cell volume from a `Lattice` or a `Cell`.
-"""
-cellvolume(lattice::Lattice) = abs(det(lattice.data))
-cellvolume(cell::Cell) = cellvolume(cell.lattice)
-
 Base.size(::Lattice) = (3, 3)
 Base.length(::Lattice) = 9  # Number of elements
 Base.getindex(A::Lattice, i::Integer, j::Integer) = getindex(A.data, i, j)
--- src/arithmetics.jl
+++ src/arithmetics.jl
@@ -4,6 +4,22 @@ abstract type AbstractSpace end
 struct RealSpace <: AbstractSpace end
 struct ReciprocalSpace <: AbstractSpace end
 
+"""
+    cellvolume(a, b, c, α, β, γ)
+
+Calculates the cell volume from 6 cell parameters.
+"""
+cellvolume(a, b, c, α, β, γ) =
+    a * b * c * sqrt(sin(α)^2 - cos(β)^2 - cos(γ)^2 + 2 * cos(α) * cos(β) * cos(γ))
+"""
+    cellvolume(l::Lattice)
+    cellvolume(c::Cell)
+
+Calculates the cell volume from a `Lattice` or a `Cell`.
+"""
+cellvolume(lattice::Lattice) = abs(det(lattice.data))
+cellvolume(cell::Cell) = cellvolume(cell.lattice)
+
 include("miller.jl")
 
 struct CartesianFromCrystal
