80e8bf98bb510f91618810099fed519a588d79f1
Reno <25192197+singularitti@users.noreply.github.com>
2021-05-26 22:41:26 -0400

    Move `Miller`, `MillerBravais`, etc., to miller.jl

--- src/arithmetics.jl
+++ src/arithmetics.jl
@@ -1,18 +1,6 @@
-export RealSpace,
-    ReciprocalSpace,
-    MetricTensor,
-    Miller,
-    MillerBravais,
-    CrystalFromCartesian,
-    CartesianFromCrystal
+export RealSpace, ReciprocalSpace, MetricTensor, CrystalFromCartesian, CartesianFromCrystal
 export directioncosine,
-    directionangle,
-    distance,
-    interplanar_spacing,
-    reciprocal,
-    cellparameters,
-    @m_str,
-    @mb_str
+    directionangle, distance, interplanar_spacing, reciprocal, cellparameters
 
 abstract type AbstractSpace end
 struct RealSpace <: AbstractSpace end
@@ -33,55 +21,7 @@ function MetricTensor(a, b, c, α, β, γ)
     return MetricTensor(SHermitianCompact(SVector(a^2, g12, g13, b^2, g23, c^2)))
 end
 
-struct Miller{S<:AbstractSpace} <: AbstractVector{Int}
-    data::SVector{3,Int}
-    Miller{S}(v) where {S} = new(iszero(v) ? v : v .÷ gcd(v))
-end
-Miller{S}(i, j, k) where {S} = Miller{S}([i, j, k])
-
-struct MillerBravais{S<:AbstractSpace} <: AbstractVector{Int}
-    data::SVector{4,Int}
-    function MillerBravais{S}(v) where {S}
-        @assert(
-            v[3] == -v[1] - v[2],
-            "the 3rd index of `MillerBravais` should equal to the negation of the first two!"
-        )
-        return new(iszero(v) ? v : v .÷ gcd(v))
-    end
-end
-MillerBravais{S}(i, j, k, l) where {S} = MillerBravais{S}([i, j, k, l])
-
-# This is a helper type and should not be exported!
-const INDICES = Union{Miller,MillerBravais}
-
-# This is a helper function and should not be exported!
-function _indices_str(r::Regex, s::AbstractString, ::Type{T}) where {T<:INDICES}
-    m = match(r, strip(s))
-    if m === nothing
-        error("not a valid expression!")
-    else
-        brackets = first(m.captures) * last(m.captures)
-        x = (parse(Int, x) for x in m.captures[2:(end-1)])
-        if brackets ∈ ("()", "{}")
-            return T{ReciprocalSpace}(x...)
-        elseif brackets ∈ ("[]", "<>")
-            return T{RealSpace}(x...)
-        else
-            error("not a valid expression!")
-        end
-    end
-end
-
-macro m_str(s)
-    r = r"([({[<])\s*([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]*([>\]})])"
-    _indices_str(r, s, Miller)
-end
-
-macro mb_str(s)
-    r =
-        r"([({[<])\s*([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]+([-+]?[0-9]+)[\s,]*([>\]})])"
-    _indices_str(r, s, MillerBravais)
-end
+include("miller.jl")
 
 struct CartesianFromCrystal
     m::SMatrix{3,3}
@@ -170,27 +110,14 @@ distance(a::AbstractVector, g::MetricTensor, b::AbstractVector) = norm(b - a, g)
 interplanar_spacing(a::AbstractVector, g::MetricTensor) = 1 / norm(a, g)
 
 Base.size(::MetricTensor) = (3, 3)
-Base.size(::Miller) = (3,)
-Base.size(::MillerBravais) = (4,)
 
 Base.getindex(A::MetricTensor, I::Vararg{Int}) = getindex(A.data, I...)
-Base.getindex(A::Union{Miller,MillerBravais}, i::Int) = getindex(A.data, i)
 
 Base.inv(g::MetricTensor) = MetricTensor(inv(g.data))
 Base.inv(x::CrystalFromCartesian) = CartesianFromCrystal(inv(x.m))
 Base.inv(x::CartesianFromCrystal) = CrystalFromCartesian(inv(x.m))
 Base.:∘(x::CrystalFromCartesian, y::CartesianFromCrystal) =
     x.m * y.m ≈ I ? IdentityTransformation() : error("undefined!")
-
-Base.convert(::Type{T}, x::T) where {T<:INDICES} = x
-Base.convert(::Type{Miller{T}}, mb::MillerBravais{T}) where {T<:RealSpace} =
-    Miller{T}(2 * mb[1] + mb[2], 2 * mb[2] + mb[1], mb[4])
-Base.convert(::Type{Miller{T}}, mb::MillerBravais{T}) where {T<:ReciprocalSpace} =
-    Miller{T}(mb[1], mb[2], mb[4])
-Base.convert(::Type{MillerBravais{T}}, m::Miller{T}) where {T<:RealSpace} =
-    MillerBravais{T}(2 * m[1] - m[2], 2 * m[2] - m[1], -(m[1] + m[2]), 3 * m[3])
-Base.convert(::Type{MillerBravais{T}}, m::Miller{T}) where {T<:ReciprocalSpace} =
-    MillerBravais{T}(m[1], m[2], -(m[1] + m[2]), m[3])
 
 dot(a::AbstractVector, g::MetricTensor, b::AbstractVector) = a' * g.data * b
 norm(a::AbstractVector, g::MetricTensor) = sqrt(dot(a, g, a))
