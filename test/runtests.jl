19283ab4d375302c1488fba31a7156e9e1f00734
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-08 03:51:58 -0400

    Remove some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,4 @@ using Crystallography
 using Test
 
 @testset "Crystallography.jl" begin
-    using Crystallography
-    using Test
-    include("miller.jl")
-    include("metric.jl")
-    include("transform.jl")
-    include("reciprocal.jl")
 end
