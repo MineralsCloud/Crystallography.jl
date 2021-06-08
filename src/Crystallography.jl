a39eae27528f0952fa82c95a0fe0a01523b97908
Reno <25192197+singularitti@users.noreply.github.com>
2021-06-08 03:51:24 -0400

    Remove some source files in src/

--- src/Crystallography.jl
+++ src/Crystallography.jl
@@ -1,17 +1,5 @@
 module Crystallography
 
-using LinearAlgebra: det
-using StaticArrays: SVector, SMatrix
-
-import LinearAlgebra: dot, norm
-import Spglib: basis_vectors
-
-include("lattice.jl")
-include("reciprocal.jl")
-include("miller.jl")
-include("metric.jl")
-include("volume.jl")
-include("transform.jl")
 include("Symmetry.jl")
 
 end
