9abfef7c560d2486e6860d9347d895b2f9ac1584
Reno <25192197+singularitti@users.noreply.github.com>
2021-11-06 16:42:11 -0400

    Add methods `hermann_mauguin` & `international`

--- src/PointGroups.jl
+++ src/PointGroups.jl
@@ -8,7 +8,7 @@ using ..Crystallography: Triclinic,
     Trigonal,
     Hexagonal
 
-export pointgroups
+export pointgroups, hermann_mauguin, international
 
 abstract type PointGroup end
 abstract type CyclicGroup <: PointGroup end
@@ -66,5 +66,39 @@ pointgroups(::Tetragonal) = (C4(), S4(), C4h(), D4(), C4v(), D2d(), D4h())
 pointgroups(::Trigonal) = (C3(), S6(), D3(), C3v(), D3d())
 pointgroups(::Hexagonal) = (C6(), C3h(), C6h(), D6(), C6v(), D3h(), D6h())
 pointgroups(::Cubic) = (T(), Th(), O(), Td(), Oh())
+
+hermann_mauguin(::C1) = "1"
+hermann_mauguin(::S2) = "1̄"
+hermann_mauguin(::C2) = "2"
+hermann_mauguin(::C1h) = "m"
+hermann_mauguin(::C3) = "3"
+hermann_mauguin(::C4) = "4"
+hermann_mauguin(::S4) = "4̄"
+hermann_mauguin(::C2h) = "2/m"
+hermann_mauguin(::D2) = "222"
+hermann_mauguin(::C2v) = "mm2"
+hermann_mauguin(::S6) = "3̄"
+hermann_mauguin(::C6) = "6"
+hermann_mauguin(::C3h) = "6̄"
+hermann_mauguin(::D3) = "32"
+hermann_mauguin(::C3v) = "3m"
+hermann_mauguin(::D2h) = "mmm"
+hermann_mauguin(::C4h) = "4/m"
+hermann_mauguin(::D4) = "422"
+hermann_mauguin(::C4v) = "4mm"
+hermann_mauguin(::D2d) = "4̄2m"
+hermann_mauguin(::C6h) = "6/m"
+hermann_mauguin(::T) = "23"
+hermann_mauguin(::D3d) = "3̄m"
+hermann_mauguin(::D6) = "622"
+hermann_mauguin(::C6v) = "6mm"
+hermann_mauguin(::D3h) = "6̄m2"
+hermann_mauguin(::D4h) = "4/mmm"
+hermann_mauguin(::D6h) = "6/mmm"
+hermann_mauguin(::Th) = "m3̄"
+hermann_mauguin(::O) = "432"
+hermann_mauguin(::Td) = "4̄3m"
+hermann_mauguin(::Oh) = "m3̄m"
+const international = hermann_mauguin
 
 end
