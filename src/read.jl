c9351e2526b8fdb9e5e0424fafe04cab90748e5a
singularitti <singularitti@outlook.com>
2023-03-15 01:50:21 -0400

    Add type parameter `T` in `readcif`

--- src/read.jl
+++ src/read.jl
@@ -2,8 +2,25 @@ using CrystalInfoFramework: Cif, get_loop
 
 export readcif
 
-readcif(filename) = open(io -> readcif(io), filename)
-function readcif(io::IO)
+readcif(filename, ::Type{T}) where {T} = open(io -> readcif(io, T), filename)
+function readcif(io::IO, ::Type{Lattice})
+    str = read(io, String)
+    cif = Cif(str)
+    block = first(cif).second
+    a, b, c, α, β, γ = map((
+        "_cell_length_a",
+        "_cell_length_b",
+        "_cell_length_c",
+        "_cell_angle_alpha",
+        "_cell_angle_beta",
+        "_cell_angle_gamma",
+    )) do key
+        value = only(block[key])
+        parse(Float64, value)
+    end
+    return Lattice(a, b, c, α, β, γ)
+end
+function readcif(io::IO, ::Type{Cell})
     str = read(io, String)
     cif = Cif(str)
     block = first(cif).second
