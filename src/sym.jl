7053e72018dd61c0fb2a6e2f1e201323408a7933
singularitti <singularitti@outlook.com>
2023-08-15 03:50:29 -0400

    Add `istranslation` & `ispointsymmetry`

--- src/sym.jl
+++ src/sym.jl
@@ -1,10 +1,11 @@
 module Symmetry
 
-using StaticArrays: MMatrix, Size
+using LinearAlgebra: I, diagm, det
+using StaticArrays: MMatrix
 
 import StaticArrays: similar_type
 
-export SeitzOperator
+export SeitzOperator, istranslation, ispointsymmetry
 
 struct SeitzOperator{T} <: AbstractMatrix{T}
     data::MMatrix{4,4,T,16}
@@ -25,6 +26,26 @@ function SeitzOperator(t::AbstractVector)
 end
 SeitzOperator(m::AbstractMatrix, t::AbstractVector) =
     SeitzOperator(MMatrix{4,4}(vcat(hcat(m, t), [zeros(eltype(m), 3)... one(eltype(t))])))
+
+function istranslation(op::SeitzOperator)
+    m = op.data
+    if m[1:3, 1:3] != I || !(iszero(m[4, 1:3]) && isone(m[4, 4]))
+        return false
+    end
+    return true
+end
+
+function ispointsymmetry(op::SeitzOperator)
+    m = op.data
+    if iszero(m[4, 1:3]) &&
+        iszero(m[1:3, 4]) &&
+        isone(m[4, 4]) &&
+        abs(det(m[1:3, 1:3])) == 1
+        return true
+    else
+        false
+    end
+end
 
 similar_type(::SeitzOperator, ::Type{T}) where {T} = similar_type(SeitzOperator, T)
 similar_type(::Type{<:SeitzOperator}, ::Type{T}) where {T} = SeitzOperator{T}
