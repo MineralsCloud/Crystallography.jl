a88726b88d225559daa5b27a3e9a0f1a95fa60e5
singularitti <singularitti@outlook.com>
2023-08-15 23:28:44 -0400

    Rename `getsymmetry` to `findsymmetry`

--- src/Symmetry/spglib.jl
+++ src/Symmetry/spglib.jl
@@ -1,9 +1,9 @@
 using CrystallographyCore: AbstractCell
 using Spglib: get_symmetry
 
-export getsymmetry
+export findsymmetry
 
-function getsymmetry(cell::AbstractCell, symprec=1e-5)
+function findsymmetry(cell::AbstractCell, symprec=1e-5)
     rotations, translations = get_symmetry(cell, symprec)
     return map(rotations, translations) do rotation, translation
         SeitzOperator(rotation, translation)
