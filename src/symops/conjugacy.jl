2a09bfbc791f3e422bc35ba024b08e1024a5120b
singularitti <singularitti@outlook.com>
2023-09-17 22:10:41 -0400

    Add `conjugacy_class`

--- src/symops/conjugacy.jl
+++ src/symops/conjugacy.jl
@@ -3,3 +3,5 @@ struct ConjugacyOperation{T}
 end
 # Faster than the other implementation
 (conj::ConjugacyOperation)(op::SeitzOperator) = conj.op * op * inv(conj.op)
+
+conjugacy_class(op::SeitzOperator, ops) = Set(ConjugacyOperation(opᵢ)(op) for opᵢ in ops)
