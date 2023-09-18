99bf5248718101cc042aec94d0f03d29be2923aa
singularitti <singularitti@outlook.com>
2023-09-17 22:10:47 -0400

    Add `partition`

--- src/symops/conjugacy.jl
+++ src/symops/conjugacy.jl
@@ -5,3 +5,11 @@ end
 (conj::ConjugacyOperation)(op::SeitzOperator) = conj.op * op * inv(conj.op)
 
 conjugacy_class(op::SeitzOperator, ops) = Set(ConjugacyOperation(opᵢ)(op) for opᵢ in ops)
+
+function partition(group)
+    return Set(
+        map(group) do op
+            conjugacy_class(op, filter(!=(op), group))
+        end,
+    )
+end
