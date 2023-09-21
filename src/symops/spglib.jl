9e77b83b82edaf32ff0826ecda550c80d56f4f1f
singularitti <singularitti@outlook.com>
2023-09-21 04:48:24 -0400

    Add `findstabilizer` & `findorbit`

--- src/symops/spglib.jl
+++ src/symops/spglib.jl
@@ -1,7 +1,7 @@
 using CrystallographyCore: AbstractCell
 using Spglib: get_symmetry
 
-export findsymmetry
+export findsymmetry, findstabilizer, findlittlegroup, findorbit, findstar
 
 function findsymmetry(cell::AbstractCell, symprec=1e-5)
     rotations, translations = get_symmetry(cell, symprec)
@@ -9,3 +9,19 @@ function findsymmetry(cell::AbstractCell, symprec=1e-5)
         SeitzOperator(rotation, translation)
     end
 end
+
+function findstabilizer(symops, point, rtol=eps())
+    return filter(symops) do symop
+        isapprox(symop(point), point; rtol=rtol)
+    end
+end
+const findlittlegroup = findstabilizer
+
+function findorbit(symops, point)
+    return unique(
+        map(symops) do symop
+            symop(point)
+        end,
+    )
+end
+const findstar = findorbit
